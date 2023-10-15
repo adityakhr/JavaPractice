a866b537b262b5e6a736073d4b8c37f5d360d090
Aditya Khare <aditya.khr1904@gmail.com>
2023-10-15 14:54:21 +0530

    Right2Vote_Done

--- Right2Vote_Company_Assignment/src/com/right2Vote/CombinationFindingClass.java
+++ Right2Vote_Company_Assignment/src/com/right2Vote/CombinationFindingClass.java
@@ -2,19 +2,19 @@ package com.right2Vote;
 
 public class CombinationFindingClass {
 	public static void main(String[] args) {
-//	GIVEN:
+//		GIVEN
         double amount = 600.00;
         double priceOfTShirt = 50.00;
         double priceOfJeans = 80.00;
         double priceOfJacket = 100.00;
         
-//      INVOKING A FUNCTION OF VOID TYPE FOR PRINTING THE COMBINATIONS:
+//      INVOKING A FUNCTION OF VOID TYPE FOR PRINTING THE COMBINATIONS
         combinations(amount, priceOfTShirt, priceOfJeans, priceOfJacket);
 	}
 
 //  DEFINING THE FUNCTION BELOW:
 	public static void combinations(double amount, double priceOfTShirt, double priceOfJeans, double priceOfJacket) {
-        int count=0; //TAKING THE COUNT VARIABLE FOR JUST COUNTING COMBINATIONS IN PRINT STATEMENT...
+        int count=0; //TAKING THE COUNT VARIABLE FOR JUST COUNTING COMBINATIONS IN PRINT STATEMENT
 		for (int tShirts=1; tShirts*priceOfTShirt<=amount; ++tShirts) {
 			double remainingAmountAfterTShirt=amount-(tShirts*priceOfTShirt);
             for (int jeans=1; jeans*priceOfJeans<=remainingAmountAfterTShirt; ++jeans) {
